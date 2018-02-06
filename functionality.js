047b14a0a7f70f60947c13f3d47915a9a07bb68b
Darz_K <darrenkellydk@rogers.com>
2018-02-05 21:01:37 -0500

    added controller object

--- functionality.js
+++ functionality.js
@@ -36,12 +36,17 @@ let model = {
 			let index = ship.locations.indexOf(guess); //if guess is in the locations array itz a hit
 			if (index >= 0) {
 				ship.hits[index] = "hit"; //mark index of hits array as a hit
+				view.displayHit(guess); //notify view of hit at that location
+				view.displayMessage('HiT!'); //display message from view
 				if (this.isSunk(ship)) {
+					view.displayMessage('You sank my battleship');
 					this.shipsSunk++;
 				}
 				return true; // return true for a hit
 			}
 		}
+		view.displayMiss(guess);
+		view.displayMessage('You missed');
 		return false;// Otherwize return a false/'miss'
 	},
 
@@ -55,3 +60,47 @@ let model = {
 		return true;
 	}
 };
+// --testcode
+// model.fire("53");
+// model.fire("06");
+// model.fire("16");
+// model.fire("26");
+// model.fire("34");
+// model.fire("24");
+// model.fire("44");
+// model.fire("12");
+// model.fire("11");
+// model.fire("10");
+let controller = {
+	guesses : 0,
+	processGuess : function(guess) {
+
+	}
+};
+//check validty of guess - length and type of input
+function parseGuess(guess) {
+	const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+
+	if (guess === null || guess.length !== 2) {
+		alert('Oopz, Please enter a letter and a number');
+	} else {
+		firstChar = guess.charAt(0);
+		let row = alphabet.indexOf(firstChar);
+		let column = guess.charAt(1); //grab 2nd char in string representing the column
+
+		if (isNaN(row) || isNaN(column)) { //check if row or column is NAN
+			alert('Oopz, that iznt on the board');
+		} else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize){
+			alert('Oopz, thatz off the board');
+		} else {
+			return row + column;
+		}
+	}
+	return null;
+}
+//  --testcode
+// console.log(parseGuess("A0"));
+// console.log(parseGuess("B6"));
+// console.log(parseGuess("G3"));
+// console.log(parseGuess("H0"));
+// console.log(parseGuess("A7"));
